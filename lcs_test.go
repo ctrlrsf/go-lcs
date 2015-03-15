488faad8de8c9923be10db7fb92799c3f8bcc792
Rene Fragoso <ctrlrsf@gmail.com>
2015-03-15 11:35:40 -0400

    Update test to check length and actual sequence

--- lcs_test.go
+++ lcs_test.go
@@ -2,26 +2,31 @@ package main
 
 import "testing"
 
-type LCSLengthTest struct {
-	first  string
-	second string
-	count  int
+type LCSTest struct {
+	first    string
+	second   string
+	length   int
+	sequence string
 }
 
-func TestLCSLength(t *testing.T) {
-	positives := []LCSLengthTest{
-		LCSLengthTest{"abc", "abc", 3},
-		LCSLengthTest{"onetwothreefour", "onetwothreefour", 15},
-		LCSLengthTest{"one", "six", 0},
-		LCSLengthTest{"abc", "defg", 0},
-		LCSLengthTest{"hijk", "lmn", 0},
+func TestLCS(t *testing.T) {
+	positives := []LCSTest{
+		LCSTest{"abc", "abc", 3, "abc"},
+		LCSTest{"onetwothreefour", "onetwothreefour", 15, "onetwothreefour"},
+		LCSTest{"one", "six", 0, ""},
+		LCSTest{"abc", "defg", 0, ""},
+		LCSTest{"hijk", "lmn", 0, ""},
 	}
 
 	for i := 0; i < 2; i++ {
 		test := positives[i]
-		lcs := LCSLength(test.first, test.second)
-		if lcs != test.count {
-			t.Fatalf("%s, %s, expected=%d, actual=%d", test.first, test.second, test.count, lcs)
+		length, sequence := LCS([]rune(test.first), []rune(test.second))
+		if length != test.length {
+			t.Fatalf("bad length: %s, %s, expected=%d, actual=%d", test.first, test.second, test.length, length)
+		}
+		if test.sequence != string(sequence) {
+			t.Fatalf("bad sequence: %s, %s, expected=%d, actual=%d",
+				test.first, test.second, test.sequence, sequence)
 		}
 	}
 }
